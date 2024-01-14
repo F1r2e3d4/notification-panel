2d3a439009ebadd4ded74857779a6b9da4fe5d77
F1r2e3d4 <leo707679@gmail.com>
2024-01-14 18:29:17 +0300

    counter

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const counter = document.getElementById('counter');
-const unmarkall = document.getElementById('clear-notifications');
 const section = document.querySelector('.notification-body');
 const alldots = section.querySelectorAll('span.dot');
 const allbackgrounds = section.querySelectorAll('div.unread-notification');
@@ -16,43 +15,6 @@ function readall() {
     alldots.forEach(section => {
         section.style.display = 'none';
     });
-    counter.style.display = 'none';
+    counter.innerHTML = '0';
 }
 
-function unmarked1(){
-    allbackgrounds[0].style.backgroundColor = '#fff'
-    alldots[0].style.display = 'none'
-        // if ((allbackgrounds[0].style.backgroundColor != '#fff') && (allbackgrounds[2].style.backgroundColor = '#fff')){
-    //     counter.innerHTML = '2';
-    // }  
-    // else if ((allbackgrounds[0].style.backgroundColor = '#fff') && (allbackgrounds[2].style.backgroundColor != '#fff')){
-    //     counter.innerHTML = '2';
-    // }
-}
-
-function unmarked2(){
-    allbackgrounds[1].style.backgroundColor = '#fff'
-    alldots[1].style.display = 'none'
-    // if ((allbackgrounds[0].style.backgroundColor != '#fff') && (allbackgrounds[2].style.backgroundColor = '#fff')){
-    //     counter.innerHTML = '2';
-    // }  
-    // else if ((allbackgrounds[0].style.backgroundColor = '#fff') && (allbackgrounds[2].style.backgroundColor != '#fff')){
-    //     counter.innerHTML = '2';
-    // }
-
-}
-function unmarked3(){
-    allbackgrounds[2].style.backgroundColor = '#fff'
-    alldots[2].style.display = 'none';
-    // if ((allbackgrounds[1].style.backgroundColor != '#fff') && (allbackgrounds[0].style.backgroundColor = '#fff')){
-    //     counter.innerHTML = '2';
-    // }  
-    // else if ((allbackgrounds[1].style.backgroundColor = '#fff') && (allbackgrounds[0].style.backgroundColor != '#fff')){
-    //     counter.innerHTML = '2';
-    // }
-}
-allbackgrounds[0].addEventListener('click', unmarked1);
-allbackgrounds[1].addEventListener('click', unmarked2);
-allbackgrounds[2].addEventListener('click', unmarked3);
-
- 
